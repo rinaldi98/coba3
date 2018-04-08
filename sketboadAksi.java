a823951362d8ad4e0d5d6e4daa1c78852076efb0
rinaldi <akhmadaldi@gmail.com>
2018-04-08 18:56:43 +0700

    buat getter dan setter

--- sketboadAksi.java
+++ sketboadAksi.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package coba3;
 
-/**
- *
- * @author user
- */
 public class sketboadAksi {
-    
+    public static void main(String[] args){
+        sketboad A = new sketboad();  
+   
+        A.setMerk("rifcul");
+        A.setWarna("Merah");
+        A.setHarga(170000);
 }
