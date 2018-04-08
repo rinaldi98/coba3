296e07e5f237d7b84b610259db49b4dd40369876
rinaldi <akhmadaldi@gmail.com>
2018-04-08 18:58:03 +0700

    isi atribut dan panggil cetak info

--- sketboadAksi.java
+++ sketboadAksi.java
@@ -7,4 +7,19 @@ public class sketboadAksi {
         A.setMerk("rifcul");
         A.setWarna("Merah");
         A.setHarga(170000);
-}
+
+        A.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(A.getmerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(A.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(A.getHarga());
+        
+        sketboad AP = new sketboad();
+        AP.setMerk("oakey");
+        AP.setWarna("Hitam");
+        AP.setHarga(1200000);
+     }
+  }
