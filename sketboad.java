8edb53eab53c84a5d75a62229ead002c09c1cbe1
rinaldi <akhmadaldi@gmail.com>
2018-04-08 18:53:42 +0700

    method cetak info

--- sketboad.java
+++ sketboad.java
@@ -4,4 +4,10 @@ public class sketboad {
   private String merk;
   private String warna;
   private double harga;
+
+ void cetakInfo(){
+       System.out.println("Merk \t:    "+merk+"\n"+
+                           "Warna \t:   "+warna+"\n"+
+                           "Harga  \t:"+harga);
+  
 }
